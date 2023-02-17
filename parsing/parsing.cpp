403e954abbcac222d31e863b113dd8b76f19d4d6
mbjaghou <mouhamedbj61@gmail.com>
2023-02-17 20:02:41 +0100

    parsing

--- parsing/parsing.cpp
+++ parsing/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: mbjaghou <mbjaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 14:23:34 by mbjaghou          #+#    #+#             */
-/*   Updated: 2023/02/17 18:28:48 by mbjaghou         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:57:42 by mbjaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,34 @@ void pars::open_file_read(char **av)
             if (!line.find("server" , 0, 6))
                 count_server++;
             if (!line.find("    server_name", 0 , 14))
-                server_name.push_back(line);
+            {
+                std::string tmp = line.substr(15);
+                server_name.push_back(tmp);
+                
+            }
             if (!line.find("    listen", 0, 10))
-                listen.push_back(line);
+            {
+                std::string tmp = line.substr(11);
+                listen.push_back(tmp);
+            }
             if (!line.find("    root", 0, 8))
-                root.push_back(line);
-            if (!line.find("    root", 0, 8))
-                root.push_back(line);
+            {
+                std::string tmp = line.substr(9);
+                root.push_back(tmp);
+            }
             if (!line.find("    error_page", 0, 14))
-                error_page.push_back(line);
+            {
+                std::string tmp = line.substr(15);
+                error_page.push_back(tmp);
+            }
             if (!line.find("    index", 0, 9))
-                index.push_back(line);
+            {
+                std::string tmp = line.substr(10);
+                index.push_back(tmp);
+            }
             config.push_back(line);
         }
+        file.close();
     }
     else
         throw std::invalid_argument("bad Open File");
